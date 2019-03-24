971e952051ce53c97bba9a20f078cbdae9f0a5eb
berkturan <berk.turan2@isik.edu.tr>
2019-03-25 00:55:12 +0300

    Initial Commit

---
+++ TurkishKit-UIPageControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TurkishKit-UIPageControl
+//
+//  Created by Berk Turan on 25.03.2019.
+//  Copyright © 2019 Berk Turan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
