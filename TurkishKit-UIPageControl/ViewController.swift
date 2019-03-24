6fd33ea7234060949ef28302e92b2b8f37e066ab
berkturan <berk.turan2@isik.edu.tr>
2019-03-25 02:31:36 +0300

    Initial Commit

--- TurkishKit-UIPageControl/ViewController.swift
+++ TurkishKit-UIPageControl/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var myLabel: UILabel!
+    @IBAction func changePage(_ sender: UIPageControl) {
+        
+        myLabel.text = "Current Page : \(sender.currentPage + 1)"
+        
+        switch sender.currentPage {
+        case 0:
+            self.view.backgroundColor = UIColor.blue
+        case 1:
+            self.view.backgroundColor = UIColor.gray
+        case 2:
+            self.view.backgroundColor = UIColor.orange
+        default:
+            self.view.backgroundColor = UIColor.brown
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        pageControl.numberOfPages = 4
+        pageControl.pageIndicatorTintColor = UIColor.yellow
+        pageControl.currentPageIndicatorTintColor = UIColor.blue
+        pageControl.addTarget(self, action: #selector(changePage(_:)), for: .allTouchEvents)
     }
-
+    
+    
 
 }
 
